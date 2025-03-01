296a2533d7438e648422efd341626a71a5973d84
maahma <maahaahmad99@gmail.com>
2025-03-01 18:53:54 +0000

    configuring auth0 for log inout

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 const emotionRoutes = require("./routes/emotion_log")
+const { auth } = require('express-openid-connect');
 
 // app
 const app = express();
@@ -19,9 +20,22 @@ app.use((req, res, next) => {
     next()
 })
 
+// auth0
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.ISSUE_BASE_URL,
+    secret: process.env.SECRET
+  };
+
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
 // routes
 app.use('/api/emotions', emotionRoutes)  // attaches all the routes related to a emotion
-
 
 //connect to db
 mongoose.connect(process.env.DB_URI).then(() => {
